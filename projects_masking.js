f6d74cd33f3a7f8e3f20c81f151f79c3baed2eed
dharana07 <146009541+dharana07@users.noreply.github.com>
2023-09-25 21:18:52 +0530

    Update projects_masking.js

--- projects_masking.js
+++ projects_masking.js
@@ -1,26 +1,6 @@
-var projectNames = [
-                    'woolwortau', 'mdlzrusf', 'pngmx', 'pngbr',
-                    'gskjp', 'ccaau', 'beiersdorfco', 'sinoth', 'diageous', 'pepsicoes',
-                    'sanofies', 'diageotw'
-                ];
-(function () {
-                    'use strict';
-                    setTimeout(function () {
-                        setInterval(function () {
-                            var element = document.querySelector('.queueSelected');
-
-                            if (element && element.textContent.trim() === 'Masking') {
-                                console.log('yes');
-                                $.each(projectNames, function (index, projectName) {
-                                    $(".project-name.trax-tst-homepage-queue-Fbox-title:contains('" + projectName + "')").closest(".homepage-queue-box-container").css("background-color", "#FF5757");
-                                });
-                            } else {
-                                $.each(projectNames, function (index, projectName) {
-                                    $(".project-name.trax-tst-homepage-queue-Fbox-title:contains('" + projectName + "')").closest(".homepage-queue-box-container").css("background-color", "#eee");
-                                });
-                                console.log('no');
-                            }
-                        }, 1000 * 2);
-                    }, 8);
-
-                })();
+// Store the projectNames array in Tampermonkey's storage
+GM_setValue("projectNames", [
+    'woolwortau', 'mdlzrusf', 'pngmx', 'pngbr',
+    'gskjp', 'ccaau', 'beiersdorfco', 'sinoth', 'diageous', 'pepsicoes',
+    'sanofies', 'diageotw'
+]);
