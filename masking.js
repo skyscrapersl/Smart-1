284fb6818d200a795b3bb4188ca21da81407ce0b
dharana07 <146009541+dharana07@users.noreply.github.com>
2023-09-25 21:06:17 +0530

    Update masking.js

--- masking.js
+++ masking.js
@@ -7,3 +7,24 @@ var projectNames = [
                     'gskjp', 'ccaau', 'beiersdorfco', 'sinoth', 'diageous', 'pepsicoes',
                     'sanofies', 'diageotw'
                 ];
+(function () {
+    'use strict';
+    setTimeout(function () {
+        setInterval(function () {
+            var element = document.querySelector('.queueSelected');
+
+            if (element && element.textContent.trim() === 'Masking') {
+                console.log('yes');
+                $.each(projectNames, function (index, projectName) {
+                    $(".project-name.trax-tst-homepage-queue-Fbox-title:contains('" + projectName + "')").closest(".homepage-queue-box-container").css("background-color", "#FF5757");
+                });
+            } else {
+                $.each(projectNames, function (index, projectName) {
+                    $(".project-name.trax-tst-homepage-queue-Fbox-title:contains('" + projectName + "')").closest(".homepage-queue-box-container").css("background-color", "#eee");
+                });
+                console.log('no');
+            }
+        }, 1000 * 2);
+    }, 8);
+
+})();
